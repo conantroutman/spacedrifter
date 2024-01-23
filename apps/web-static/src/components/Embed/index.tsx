6ddb1e60311658ac8a1d56aeb0083c0c90b4f2d5
John Söderberg <38869877+conantroutman@users.noreply.github.com>
2024-01-23 12:53:06 +0100

    Updated Spotify embed

--- apps/web-static/src/components/Embed/index.tsx
+++ apps/web-static/src/components/Embed/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './Embed.module.css';
+import React from "react";
+import styles from "./Embed.module.css";
 
 export const Embed = () => {
   return (
